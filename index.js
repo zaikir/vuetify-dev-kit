fd3857b80ff68ffec673d273853f39d0b3c8c80c
Zaidulin Kirill <zaidulinkirill@gmail.com>
2019-10-21 13:05:04 +0300

    Fix import

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import DataTable from './src/components/DataTable'
 import EditItemDialog from './src/components/EditItemDialog'
-
-export { default as DataTable } from './src/components/DataTable'
-export { default as EditItemDialog } from './src/components/EditItemDialog'
-export { default as ConfirmationDialog } from './src/components/ConfirmationDialog'
+import ConfirmationDialog from './src/components/ConfirmationDialog'
 
 export default ({
   install: (Vue, options) => {
     Vue.component('DataTable', DataTable)
     Vue.component('EditItemDialog', EditItemDialog)
+    Vue.component('ConfirmationDialog', ConfirmationDialog)
   }
 })
