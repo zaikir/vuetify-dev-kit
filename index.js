0375f424a7a3c3244c7ab9756b45a2f1009cc88a
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-02-06 12:27:33 +1000

    Fix

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import generateForm from 'vue-generic-form'
-// import EditItemDialog from './src/components/EditItemDialog'
-// import ConfirmationDialog from './src/components/ConfirmationDialog'
 export { default as withDoubleClickEventDataTable } from './src/withDoubleClickEventDataTable'
 export { default as withHasuraDataTable } from './src/withHasuraDataTable'
-
-// export { default as buildForm } from './src/buildForm'
 
 export const buildForm = generateForm
 export const GenericForm = generateForm()
