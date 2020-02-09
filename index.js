ce9a8deeb29daa9a049a143da98754737d55edee
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-02-09 23:02:18 +1000

    Make HasuraEditItemDialog public

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { withDoubleClickEventDataTable, withApolloEditForm } from './src/hoc'
 export { default as HasuraDataTable } from './src/withHasuraDataTable'
+export { default as HasuraEditItemDialog } from './src/HasuraEditItemDialog'
