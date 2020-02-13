0dcd84e38aa87713255250ac17b4563decb5e5b5
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-02-13 11:07:25 +1000

    Fix

--- src/withHasuraDataTable/components/SelectCell.vue
+++ src/withHasuraDataTable/components/SelectCell.vue
@@ -3,12 +3,12 @@ export default {
   functional: true,
   props: {
     value: String,
-    options: Array
+    items: Array
   },
   render (createElement, context) {
     if (!context.props.value) { return createElement('span', '—') }
 
-    const elem = (context.props.options || []).find(x => x[context.props.itemValue || 'value'] === context.props.value)
+    const elem = (context.props.items || []).find(x => x[context.props.itemValue || 'value'] === context.props.value)
     if (!elem) { return '—' }
 
     return createElement('span', elem[context.props.itemText || 'text'])
