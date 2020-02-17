407b4f67ec3d0478ba8b55b27f9472464e15c5ad
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-02-17 18:05:32 +1000

    Update version

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 export { withDoubleClickEventDataTable, withApolloEditForm } from './src/hoc'
 export { default as HasuraDataTable } from './src/withHasuraDataTable'
 export { default as HasuraEditItemDialog } from './src/shared/components/HasuraEditItemDialog'
-export { default as HasuraEditForm } from './src/shared/components/HasuraEditForm'
